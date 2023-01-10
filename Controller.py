8e43694c8016ac082542cc93c14f2ad66cd52d82
Luísa <112406093+moonlight93-svg@users.noreply.github.com>
2023-01-10 09:08:40 -0300

    Add files via upload

---
+++ Controller.py
@@ -0,0 +1,53 @@
+import tkinter
+
+class Controller():
+    
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+        self.view.setCommandSearch(self.buscarSensor)
+        self.view.setCommandInsert(self.insertSensor)
+        self.view.setCommandDelete(self.deleteSensor)
+        self.view.setCommandUpdate(self.update_sensor)
+        #Chame o méotdo que que você criou para atualizar o dado aqui!
+
+        self.view.setCommandCommit(self.commitBanco)
+        
+    def buscarSensor(self):
+        idSensor = self.view.txtidSensor.get()
+        sensor  = self.model.read_one_sensor(idSensor)
+        self.view.updateBySearch(sensor)
+    
+    def insertSensor(self):
+        idSensor = self.view.txtidSensor.get()
+        variavel = self.view.txtVariavel.get()
+        medicao = self.view.txtMedicao.get()
+        unidade = self.view.txtUnidade.get()
+        registro = self.view.txtRegistro.get()
+        latitude = self.view.txtLatitude.get()
+        longitude = self.view.txtLongitude.get()
+        
+        self.model.insert_one_sensor(idSensor, variavel, medicao, unidade, registro, latitude, longitude)
+        self.view.logUpdate(idSensor)
+    
+    def deleteSensor(self):
+        idSensor = self.view.txtidSensor.get()
+        self.model.delete_one_sensor(idSensor)
+        self.view.delete_sensor(idSensor)
+        
+    def update_sensor(self):
+        idSensor = self.view.txtidSensor.get()
+        variavel = self.view.txtVariavel.get()
+        medicao = self.view.txtMedicao.get()
+        unidade = self.view.txtUnidade.get()
+        registro = self.view.txtRegistro.get()
+        latitude = self.view.txtLatitude.get()
+        longitude = self.view.txtLongitude.get()
+        
+        self.model.update_one_sensor(idSensor, variavel, medicao, unidade, registro, latitude, longitude)
+        self.view.atualizar_sensor(idSensor)
+        
+    #Crie aqui o método para atualizar os dados
+        
+    def commitBanco(self):
+        self.model.commit_changes()
